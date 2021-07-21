6f475c4c86d3b48f3c25a8b890d54f0ee65263a9
Anton Tsukanov <tsukanov@bionet.nsc.ru>
2021-07-21 21:21:12 +0700

    fix

--- lib/functions.pyx
+++ lib/functions.pyx
@@ -6,9 +6,20 @@ def read_fasta(str path):
     with open(path, 'r') as file:
         for line in file:
             if not line.startswith('>'):
+                seq = line.strip().upper()
                 fasta.append(line.strip().upper())
     file.close()
     return(fasta)
+
+
+def complement(seq):
+    seq = seq.replace('A', 't')
+    seq = seq.replace('T', 'a')
+    seq = seq.replace('C', 'g')
+    seq = seq.replace('G', 'c')
+    seq = seq.upper()
+    seq = seq[::-1]
+    return(seq)
 
 
 def shuffle_fasta(list fasta, int times):
