3946a6315b954c443828c675dfc960bf8bd176f2
Anton Tsukanov <ubercomrade@gmail.com>
2022-04-15 22:53:15 +0700

    background

--- cenrom.py
+++ cenrom.py
@@ -12,8 +12,10 @@ def parse_args():
     parser.add_argument('matrix', action='store', help='path to MATRIX file (PCM or PFM depend on source)')
     parser.add_argument('promoters', action='store', choices=['mm10', 'hg38', 'tair10', 'b73v5', 'rnor6'], metavar='N',
          help='promoters of organism (hg38, mm10, tair10, rnor6, b73v5)')
+    parser.add_argument('-b', '--background', action='store', type=str, dest='background',
+                        required=False, default='shuffled', help='Path to background. if not given peaks are shuffled')
     parser.add_argument('-n', '--ntimes', action='store', default=100, type=int, dest='ntimes',
-        help='N times FASTA for background')
+        help='N times FASTA for background. It`s used when "--background" is not given')
     parser.add_argument('-f', '--format', action='store', choices=['homer', 'cisbp', 'hocomoco'], metavar='N',
         dest='format', default='homer', help='[homer, cisbp, hocomoco] format of input PFM (HOMER, CISBP) or PCM (HOCOMOCO)')
     parser.add_argument('-t', '--threshold', action='store', type=float, dest='threshold',
@@ -27,6 +29,7 @@ def parse_args():
 def main():
     args = parse_args()
     fasta_path = args.fasta
+    background_path = args.background
     matrix_path = args.matrix
     organism = args.promoters
     ntimes = args.ntimes
@@ -46,7 +49,10 @@ def main():
         path_to_promoters = os.path.join(this_dir, "promoters", "rnor6.fasta")
 
     fasta = read_fasta(fasta_path)
-    background = shuffle_fasta(fasta, ntimes)
+    if os.path.exists(background_path):
+        background = read_fasta(background_path)
+    else:
+        background = shuffle_fasta(fasta, ntimes)
     pwm = PWM(matrix_path, form=matrix_format)
     pwm.calculate_table(path_to_promoters)
     threshold = pwm.choose_threshold(fpr_threshold)
